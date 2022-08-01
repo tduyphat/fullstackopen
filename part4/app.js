3e9c97badf92ae682d4cad7b5db1ad33c91d027c
unknown <tranduyphat080300@gmail.com>
2022-08-01 23:15:54 +0700

    add users controller, user model

--- part4/app.js
+++ part4/app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
 
 
 logger.info('connecting to', config.mongoUrl)
@@ -21,11 +22,12 @@ mongoose.connect(config.mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(middleware.requestLogger)
 
 module.exports = app
