6272ae65a267c2495741bcd4f36fea65cfebec72
unknown <tranduyphat080300@gmail.com>
2023-02-03 15:20:17 +0200

    7.8

---
+++ part7/ultimate-hooks/src/App.js
@@ -0,0 +1,51 @@
+import React  from 'react'
+import { useField } from './hooks/useField'
+import { useResource } from './hooks/useResource'
+
+const App = () => {
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+    content.reset()
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
+    name.reset()
+    number.reset()
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input
+         type={content.type} 
+         value={content.value} 
+         onChange={content.onChange} 
+         required
+        />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input type={name.type} value={name.value} onChange={name.onChange} required/> <br/>
+        number <input type={number.type} value={number.value} onChange={number.onChange} required/>
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App
---
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+export const useResource = (baseUrl) => {
+    const [resources, setResources] = useState([])
+    useEffect(() => {
+        axios.get(baseUrl).then(response => {
+            setResources(response.data)
+        })
+    }, [baseUrl])
+
+    const create = async (resource) => {
+      const response = await axios.post(baseUrl, resource)
+      setResources(resources.concat(response.data))
+    }
+
+    const service = {
+      create
+    }
+
+    return [
+      resources, service
+    ]
+}
