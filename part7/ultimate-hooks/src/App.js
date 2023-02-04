e1b56214b3598769aa1ff9337ea98a79c82bcd35
unknown <tranduyphat080300@gmail.com>
2023-02-04 16:08:18 +0200

    prettier

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,51 +1,70 @@
-import React  from 'react'
-import { useField } from './hooks/useField'
-import { useResource } from './hooks/useResource'
+import React from "react";
+import { useField } from "./hooks/useField";
+import { useResource } from "./hooks/useResource";
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource("http://localhost:3005/notes");
+  const [persons, personService] = useResource("http://localhost:3005/persons");
 
   const handleNoteSubmit = (event) => {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-    content.reset()
-  }
- 
+    event.preventDefault();
+    noteService.create({ content: content.value });
+    content.reset();
+  };
+
   const handlePersonSubmit = (event) => {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
-    name.reset()
-    number.reset()
-  }
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+    name.reset();
+    number.reset();
+  };
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
         <input
-         type={content.type} 
-         value={content.value} 
-         onChange={content.onChange} 
-         required
+          type={content.type}
+          value={content.value}
+          onChange={content.onChange}
+          required
         />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input type={name.type} value={name.value} onChange={name.onChange} required/> <br/>
-        number <input type={number.type} value={number.value} onChange={number.onChange} required/>
+        name{" "}
+        <input
+          type={name.type}
+          value={name.value}
+          onChange={name.onChange}
+          required
+        />{" "}
+        <br />
+        number{" "}
+        <input
+          type={number.type}
+          value={number.value}
+          onChange={number.onChange}
+          required
+        />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part7/ultimate-hooks/src/hooks/useResource.js
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -1,23 +1,23 @@
-import {useEffect, useState} from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const useResource = (baseUrl) => {
-    const [resources, setResources] = useState([])
-    useEffect(() => {
-        axios.get(baseUrl).then(response => {
-            setResources(response.data)
-        })
-    }, [baseUrl])
+  const [resources, setResources] = useState([]);
 
-    const create = async (resource) => {
-      const response = await axios.post(baseUrl, resource)
-      setResources(resources.concat(response.data))
-    }
+  useEffect(() => {
+    axios.get(baseUrl).then((response) => {
+      setResources(response.data);
+    });
+  }, [baseUrl]);
 
-    const service = {
-      create
-    }
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource);
+    setResources(resources.concat(response.data));
+  };
 
-    return [
-      resources, service
-    ]
-}
+  const service = {
+    create,
+  };
+
+  return [resources, service];
+};
